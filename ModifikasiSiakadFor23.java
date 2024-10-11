dba5f7352b0b3bed3da6283f5996a85a9ff2c9d9
fri17da <syafridaadeauliahakim@gmail.com>
2024-10-11 12:11:30 +0700

    commit

--- ModifikasiSiakadFor23.java
+++ ModifikasiSiakadFor23.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class SiakadFor23 {
+public class ModifikasiSiakadFor23 {
     public static void main(String [] args) {
     Scanner sc = new Scanner(System.in);
-
-    double nilai, tertinggi = 100, terendah = 0;
+    int lulus = 0, tidakLulus = 0;
+    double nilai, tertinggi = 0, terendah = 100, batasLulus = 60;
 
     for (int i = 1; i <= 10; i++) {
         System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,9 +14,17 @@ public class SiakadFor23 {
         if (nilai < terendah) {
             terendah = nilai;
         }
+        if (nilai >= batasLulus){
+            lulus++;
+        } else {
+            tidakLulus++;
+        }
     }
     System.out.println("Nilai tertinggi: " + tertinggi);
     System.out.println("Nilai terendah: " + terendah);
+
+    System.out.println("Jumlah Mahasiswa lulus: " + lulus);
+    System.out.println("Jumlah Mahasiswa tidak lulus: " + tidakLulus);
 
     }
 }
---
+++ PenjualanTiket23.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PenjualanTiket23 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hargaTiket = 50000;
+        int totalTiketTerjual, totalTiket = 0, i;
+        double totalHargaPenjualan = 0;
+        i = 1;
+
+        System.out.print("Masukkan total tiket: ");
+        totalTiketTerjual = sc.nextDouble();
+        System.out.print("Total harga: " + (totalTiket * hargaTiket));
+        totalHargaPenjualan = sc.nextDouble();
+
+        while ()
+
+
+
+    }
+}
